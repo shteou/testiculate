373e0c9648b533d1d60558974580285edef4114c
Stewart Platt <shteou@gmail.com>
2021-01-17 00:16:41 +0000

    feat: detecct flaky tests
    Added a flaky test section to the results view
    A test is determined to be flaky if it has a passrate below 100%
    Flaky tests are reported in ascending order of passrate. An
    alternative to this might be to look for passrates close to 50%
    
    Refactored the results view a little to move state into the
    component's scope
    
    Fixed rendering issues by clearing state when components are
    removed

--- static/resultsView.js
+++ static/resultsView.js
@@ -1,74 +1,56 @@
-// State
-
-let resultsState = {
-    results: null
-}
-
-// Service
-
-const fetchServiceResults = function(service) {
-    return fetch('http://localhost:8080/tests/' + service)
-    .then(response => response.json())
-    .then(data => {
-        resultsState.results = data;
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
-    });
-}
-
 // Helplers
 
-const isPassed = function(testResult) {
+const isPassed = function (testResult) {
     return testResult.Failed === 0 && testResult.Errored === 0;
 }
 
-const groupBy = function(xs, key) {
-    return xs.reduce(function(rv, x) {
+const groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
         (rv[x[key]] = rv[x[key]] || []).push(x);
         return rv;
     }, {});
 };
 
-const totalPassed = function(results) {
+const totalPassed = function (results) {
     return results.filter(isPassed).length;
 }
 
-const totalFailed = function(results) {
+const totalFailed = function (results) {
     return results.filter((x) => !isPassed(x)).length;
 }
 
-const passRate = function(results) {
-    return ~~((results.filter(isPassed).length / results.length)*100);
+const passRate = function (results) {
+    return ~~((results.filter(isPassed).length / results.length) * 100);
 }
 
 // Components
 
 const ResultEntry = {
-    view: function(vnode) {
+    view: function (vnode) {
         let testResult = null;
-        if(isPassed(vnode.attrs)) {
-            testResult = m("div", {class: "result resultPassed"}, "☑");
+        if (isPassed(vnode.attrs)) {
+            testResult = m("div", { class: "result resultPassed" }, "☑");
         } else {
-            testResult = m("div", {class: "result resultFailed"}, "☒");
+            testResult = m("div", { class: "result resultFailed" }, "☒");
         }
 
-        return m("div", {class: "resultEntry"},
+        return m("div", { class: "resultEntry" },
             testResult);
     }
 }
 
 const ResultDayColumn = {
-    view: function(vnode) {
-        const args = ["div", {class: "resultDayColumn"}, m("div", {class: "resultDayColumnDate"}, vnode.attrs.date)]
+    view: function (vnode) {
+        const args = ["div", { class: "resultDayColumn" }, m("div", { class: "resultDayColumnDate" }, vnode.attrs.date)]
             .concat(vnode.attrs.entries.map(x => m(ResultEntry, x)));
         return m.apply(this, args);
     }
 }
 
 const ResultMetrics = {
-    view: function(vnode) {
+    view: function (vnode) {
         const results = vnode.attrs.results;
-        return m("div", {class: "resultMetrics"},
+        return m("div", { class: "resultMetrics" },
             m("p", "Builds: " + results.length),
             m("p", "Passed: " + totalPassed(results)),
             m("p", "Failed: " + totalFailed(results)),
@@ -76,37 +58,116 @@ const ResultMetrics = {
     }
 }
 
-const ResultEntries = {
-    oninit: function(vnode) {
-        fetchServiceResults(vnode.attrs.name)
-        .then(_ => m.redraw());
-    },
-    view: function(vnode) {
-        if(resultsState.results === null) {
-            return m("div", {class: "resultEntries"}, "Searching")
-        } else {
-            const date = new Date();
-
-            resultsState.results.forEach((x) => {
-                const batchedDate = new Date(x.CreatedAt);
-                batchedDate.setMilliseconds(~~(batchedDate.getMilliseconds()/100))
-                x.CreatedAtBatched = `${batchedDate.getHours()}${batchedDate.getSeconds()}${batchedDate.getMilliseconds()}`;
+const ResultEntries = function () {
+    let results = null;
+ 
+    const fetchServiceResults = function (service) {
+        return fetch('http://localhost:8080/tests/' + service)
+            .then(response => response.json())
+            .then(data => {
+                results = data;
+            }).catch(function (err) {
+                console.warn('Something went wrong.', err);
             });
-            const dayGroups = groupBy(resultsState.results, "CreatedAtBatched");
+    }
 
-            const args = ["div", {class: "resultEntries"}, m(ResultMetrics, {results: resultsState.results})]
-                .concat(Object.keys(dayGroups).map(x => m(ResultDayColumn, {date: x, entries: dayGroups[x]})));
-            return m.apply(this, args);
+    return {
+        oninit: function(vnode) {
+            fetchServiceResults(vnode.attrs.name)
+                .then(_ => m.redraw());
+        },
+        view: function(vnode) {
+            if (results === null) {
+                return m("div", { class: "resultEntries" }, "Searching")
+            } else {
+                const date = new Date();
+
+                results.forEach((x) => {
+                    const batchedDate = new Date(x.CreatedAt);
+                    batchedDate.setMilliseconds(~~(batchedDate.getMilliseconds() / 100))
+                    x.CreatedAtBatched = `${batchedDate.getHours()}${batchedDate.getSeconds()}${batchedDate.getMilliseconds()}`;
+                });
+                const dayGroups = groupBy(results, "CreatedAtBatched");
+
+                const args = ["div", { class: "resultEntries" }, m(ResultMetrics, { results: results })]
+                    .concat(Object.keys(dayGroups).map(x => m(ResultDayColumn, { date: x, entries: dayGroups[x] })));
+                return m.apply(this, args);
+            }
+        },
+        onbeforeremove: function(vnode) {
+            rersults = null;
         }
     }
 }
 
-const ResultsView = {
+const FlakeEntry = {
     view: function(vnode) {
+        return m("div", {class: "flakeEntry"},
+            m("p", vnode.attrs.name + ": " + vnode.attrs.passed + " " + vnode.attrs.failed));
+    }
+}
+
+const FlakeEntries = function() {
+    let passFailRate = null;
+
+    const fetchServiceExecutions = function (service) {
+        return fetch('http://localhost:8080/executions/' + service)
+            .then(response => response.json())
+            .catch(function (err) {
+                console.warn('Something went wrong.', err);
+            });
+    }
+
+    const analyseFlakeyTests = function(testExecutions) {
+        let uniqueCases = testExecutions.map((te) => te.Classname + te.Name)
+            .filter((value, index, self) => self.indexOf(value) === index);
+        
+        let passFailRate = uniqueCases.map((te) => {
+            theseExecutions = testExecutions.filter((x) => (x.Classname + x.Name) === te);
+            const passed = theseExecutions.filter((x) => x.Status === "passed").length;
+            const failed = theseExecutions.filter((x) => x.Status === "failed" || x.Status === "errored").length;
+            return {
+                "passed": passed,
+                "failed": failed,
+                passRate: ~~(100.0 * (passed / (passed + failed))),
+                name: te
+            }
+        }).filter(pfr => pfr.passRate !== 100.0)
+        .sort(((a, b) => a.passRate - b.passRate));
+        
+        return passFailRate;
+    }
+
+    return {
+        oninit: function (vnode) {
+            fetchServiceExecutions(vnode.attrs.name)
+                .then(analyseFlakeyTests)
+                .then((x) => passFailRate = x)
+                .then(_ => m.redraw());
+        },
+        view: function (vnode) {
+            if (passFailRate === null) {
+                return m("div", "Flakey tests",
+                    m("p", "Analysing"));
+            } else {
+                let args = ["div", "Flakey tests"];
+                args = args.concat(passFailRate.map((pfr) => m(FlakeEntry, pfr)));
+
+                return m.apply(this, args);
+            }
+        },
+        onbeforeremove: function(vnode) {
+            passFailRate = null;
+        }
+    }
+}
+const ResultsView = {
+    view: function (vnode) {
         const serviceName = m.route.param("name");
 
-        return m("div", {class: "resultsView"},
+        return m("div", { class: "resultsView" },
             m("p", "Service - " + serviceName),
-            m(ResultEntries, {name: serviceName}));
+            m(ResultEntries, { name: serviceName }),
+            m(FlakeEntries, { name: serviceName }));
     }
 }
